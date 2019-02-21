b1dcc16ff703b52311dc5702ef21b9749d151d0e
Kareem Moussa <kareem.moussa@coinbase.com>
2019-02-21 11:09:38 -0800

    Update assume-role to v0.3.0

--- assume-role.rb
+++ assume-role.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class AssumeRole < Formula
   homepage 'https://github.com/coinbase/assume-role'
-  url 'https://github.com/coinbase/assume-role/archive/v0.2.0.tar.gz'
-  sha256 'd737d9ddd10a74e960e608b0a1e29e61fea2655f03ba3dbc0a4a9019c0700754'
+  url 'https://github.com/coinbase/assume-role/archive/v0.3.0.tar.gz'
+  sha256 'TODO'
 
   depends_on "awscli"
   depends_on "jq"
